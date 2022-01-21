c18d5f105c2e2ee71a2299e14f2c7a35c497959a
SVETIK.LION <svetlana.lvovskaya@gmail.com>
2022-01-21 22:58:35 +0500

    ДЗ №1

--- Klepikova_Svetlana_dz_1/task_1_2.py
+++ Klepikova_Svetlana_dz_1/task_1_2.py
@@ -16,20 +16,9 @@ def sum_list_1(dataset: list) -> int:
 
 def sum_list_2(dataset: list) -> int:
     """К каждому элементу списка добавляет 17 и вычисляет сумму чисел списка, сумма цифр которых делится нацело на 7"""
-    sum_digits = 0
-    sum_num = 0
     for i in range(len(dataset)):
         dataset[i] += 17
-    for i in dataset:
-        copy_i = i
-        while copy_i > 0:
-            digit = copy_i % 10
-            sum_digits += digit
-            copy_i //= 10
-        if sum_digits % 7 == 0:
-            sum_num += i
-        sum_digits = 0
-    return sum_num
+    return sum_list_1(dataset)
 
 
 my_list = [n**3 for n in range(1, 1000, 2)] #Собрали список кубов от 1 до 1000
