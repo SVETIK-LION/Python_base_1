ccca155304e509ca00ae5711b146f9eefeded00c
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-01-30 18:02:41 +0500

    task_4_2.py add .upper()

--- task_4_2.py
+++ task_4_2.py
@@ -7,7 +7,7 @@ def currency_rates(code: str) -> Decimal:
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
     text_list = response.text.split('Valute')  # Разделяем текст по валютам
     for val in text_list:
-        if code in val:
+        if code.upper() in val:
             val_list = val.split('<')  # Разделям список валют на элементы-характеристики нужной валюты
     for i in range(len(val_list)):
         if 'Value' in val_list[i]:
@@ -18,12 +18,12 @@ def currency_rates(code: str) -> Decimal:
         if 'Nominal' in val_list[j]:
             nominal = Decimal(float(val_list[j][8:])) # Вычленяем номинал
             break
-    result_value = f'{rate / nominal} - точный курс валюты {code} по отношению к рублю' # Находим значение
+    result_value = f'{rate / nominal} - точный курс валюты {code.upper()} по отношению к рублю' # Находим значение
     return result_value
 
 
-print(currency_rates("USD"))
+print(currency_rates("usd"))
 print(currency_rates("AZN"))
-print(currency_rates("AMD"))
+print(currency_rates("Amd"))
 print(currency_rates("HUF"))
 print(currency_rates("noname"))
