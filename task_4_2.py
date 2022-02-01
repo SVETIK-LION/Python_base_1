ba3cae12259dd2ed262bd1eb55bcef2ca0989762
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-01 12:32:27 +0500

    Другой способ task_4_2.py с None

--- task_4_2.py
+++ task_4_2.py
@@ -5,24 +5,20 @@ from decimal import *
 def currency_rates(code: str) -> Decimal:
     """возвращает курс валюты `code` по отношению к рублю"""
     response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    text_list = response.text.split('Valute')  # Разделяем текст по валютам
-    for val in text_list:
-        if code.upper() in val:
-            val_list = val.split('<')  # Разделям список валют на элементы-характеристики нужной валюты
-    for i in range(len(val_list)):
-        if 'Value' in val_list[i]:
-            rate = Decimal(
-                f'{val_list[i][6:8]}.{val_list[i][9:]}')  # Убираем запятую, срезами добавляем значения валюты, конвертим в decimal
-            break
-    for j in range(len(val_list)):
-        if 'Nominal' in val_list[j]:
-            nominal = Decimal(val_list[j][8:]) # Вычленяем номинал
-            break
-    result_value = f'{rate / nominal} - точный курс валюты {code.upper()} по отношению к рублю' # Находим значение
-    return result_value
+    text_list = response.text.split('<Valute ID=')  # Разделяем текст по валютам
+    for value in text_list:
+        if code.upper() in value:
+            val = value.replace('</Value>', '<Value>') # Меняем ненужные строчки, чтобы по ним сделть сплит
+            val = val.split('<Value>') # Разделяем по строке курса валюты
+            val = Decimal(val[-2].replace(',', '.')) # Меняем точку на зпт в значении курса валюты
+            nom = value.replace('</Nominal', '<Nominal>') # То же самое только для номинала
+            nom = nom.split('<Nominal>')
+            nom = Decimal(nom[-2])
+            val_out = val/nom # Делим курс на номинал
+            return f'{val_out} точный курс валюты {code.upper()} по отношению к рублю'
 
 
-print(currency_rates("usd"))
+print(currency_rates("USD"))
 print(currency_rates("AZN"))
 print(currency_rates("Amd"))
 print(currency_rates("HUF"))
