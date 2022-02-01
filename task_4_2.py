1971b5418b33ef269c801f1917c83c7eded09527
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-01 12:52:47 +0500

    Убрала лишние строчки task_4_2.py с None

--- task_4_2.py
+++ task_4_2.py
@@ -8,12 +8,10 @@ def currency_rates(code: str) -> Decimal:
     text_list = response.text.split('<Valute ID=')  # Разделяем текст по валютам
     for value in text_list:
         if code.upper() in value:
-            val = value.replace('</Value>', '<Value>') # Меняем ненужные строчки, чтобы по ним сделть сплит
-            val = val.split('<Value>') # Разделяем по строке курса валюты
-            val = Decimal(val[-2].replace(',', '.')) # Меняем точку на зпт в значении курса валюты
-            nom = value.replace('</Nominal', '<Nominal>') # То же самое только для номинала
-            nom = nom.split('<Nominal>')
-            nom = Decimal(nom[-2])
+            # Меняем ненужные строчки, чтобы по ним сделть сплит, делаем сплит, меняем тчк на зпт, преобразуем в Decimal
+            val = Decimal(value.replace('</Value>', '<Value>').split('<Value>')[-2].replace(',', '.'))
+            # То же самое, только для номинала
+            nom = Decimal(value.replace('</Nominal', '<Nominal>').split('<Nominal>')[-2])
             val_out = val/nom # Делим курс на номинал
             return f'{val_out} точный курс валюты {code.upper()} по отношению к рублю'
 
