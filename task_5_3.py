fddb7abd11867ecbdc0aa1182199b105fc458492
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-02 23:44:25 +0500

    типа генератор, но все равно словарь task_5_3.py

--- task_5_3.py
+++ task_5_3.py
@@ -1,21 +1,22 @@
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+from itertools import zip_longest
+
+
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Аккакий', 'Аркадий', 'Кира']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
 
 def check_gen(tutors: list, klasses: list):
-    di = dict(zip(tutors, klasses))
-    gen = (di.() for key in tutors)
-    return gen
 
+    gen = {key: val for key, val in zip_longest(tutors, klasses)}
+    print(gen)
+    print(type(gen))
 
 
 generator = check_gen(tutors, klasses)
-
 # добавьте здесь доказательство, что создали именно генератор
-print(type(generator))
 for _ in range(len(tutors)):
     print(next(generator))
-# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
+next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
 
 # Решение без генератора 1
 # d = dict.fromkeys(tutors) | dict(zip(tutors, klasses))
@@ -33,3 +34,6 @@ for _ in range(len(tutors)):
 # d = {key: d.get(key) for key in tutors}
 # print(d)
 # print(type(d))
+#
+#
+# generator = check_gen(tutors, klasses)
