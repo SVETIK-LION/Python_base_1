8e159314a4176bd986451d1d18809f387a6153c5
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-07 14:37:58 +0500

    выводит код 1 task_6_3.py

--- Klepikova_Svetlana_dz_6/task_6_3.py
+++ Klepikova_Svetlana_dz_6/task_6_3.py
@@ -13,17 +13,25 @@ def prepare_dataset(path_users_file: str, path_hobby_file: str) -> dict:
     """
     list_users = []
     list_hobby = []
+    count_key = 0
+    count_val = 0
     users_file = open('users.csv', 'r', encoding='utf-8')
     for line_u in users_file:
         key = line_u.strip().replace(',', ' ')
         list_users.append(key)
+        count_key += 1
     hobby_file = open('hobby.csv', 'r', encoding='utf-8')
     for line_h in hobby_file:
         val = line_h.strip()
         list_hobby.append(val)
-    return print({key: val for key, val in zip_longest(list_users, list_hobby)}) # верните словарь, либо завершите исполнение программы кодом 1
+        count_val += 1
+    if count_key < count_val:
+        return 1
+    else:
+        return {key: val for key, val in zip_longest(list_users, list_hobby)} # верните словарь, либо завершите исполнение программы кодом 1
 
 
 dict_out = prepare_dataset('users.csv', 'hobby.csv')
 with open('task_6_3_result.json', 'w', encoding='utf-8') as fw:
     json.dump(dict_out, fw, ensure_ascii=False, indent=2)
+print(dict_out) # Вывожу словарь или единицу (если имен меньше, чем хобби)
