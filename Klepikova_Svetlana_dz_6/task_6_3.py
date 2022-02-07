bc40c12f50b1f9053831da5141fcf2b822847d8d
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-07 16:03:11 +0500

    delite space and counts task_6_3.py, add add_sale.py

--- Klepikova_Svetlana_dz_6/task_6_3.py
+++ Klepikova_Svetlana_dz_6/task_6_3.py
@@ -13,19 +13,15 @@ def prepare_dataset(path_users_file: str, path_hobby_file: str) -> dict:
     """
     list_users = []
     list_hobby = []
-    count_key = 0
-    count_val = 0
-    users_file = open('users.csv', 'r', encoding='utf-8')
-    for line_u in users_file:
-        key = line_u.strip().replace(',', ' ')
-        list_users.append(key)
-        count_key += 1
-    hobby_file = open('hobby.csv', 'r', encoding='utf-8')
-    for line_h in hobby_file:
-        val = line_h.strip()
-        list_hobby.append(val)
-        count_val += 1
-    if count_key < count_val:
+    with open('users.csv', 'r', encoding='utf-8') as users_file:
+        for line_u in users_file:
+            key = line_u.strip().replace(',', '')
+            list_users.append(key)
+    with open('hobby.csv', 'r', encoding='utf-8') as hobby_file:
+        for line_h in hobby_file:
+            val = line_h.strip()
+            list_hobby.append(val)
+    if len(list_users) < len(list_hobby):
         return 1
     else:
         return {key: val for key, val in zip_longest(list_users, list_hobby)} # верните словарь, либо завершите исполнение программы кодом 1
