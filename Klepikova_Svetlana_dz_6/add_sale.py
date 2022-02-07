19a4437e6719242012137978116fe6b982444a48
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-07 19:48:55 +0500

    working add_sale.py, bakery.csv, add show_sales.py

--- Klepikova_Svetlana_dz_6/add_sale.py
+++ Klepikova_Svetlana_dz_6/add_sale.py
@@ -2,4 +2,4 @@ import sys
 
 
 with open('bakery.csv', 'a', encoding='utf-8') as fw:
-    fw = sys.argv
+    fw.writelines(str(sys.argv[1] + '\n'))
---
+++ Klepikova_Svetlana_dz_6/show_sales.py
@@ -0,0 +1,5 @@
+import sys
+
+
+with open('bakery.csv', 'r', encoding='utf-8') as fw:
+    print(fw.read())
