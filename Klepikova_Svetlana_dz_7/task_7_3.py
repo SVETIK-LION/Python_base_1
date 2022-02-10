156c5b4b3b1073170ccbdcd06a8107a64a0ca021
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-11 00:48:39 +0500

    change task_7_3.py

--- Klepikova_Svetlana_dz_7/task_7_3.py
+++ Klepikova_Svetlana_dz_7/task_7_3.py
@@ -4,8 +4,7 @@ import shutil
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 my_project_path = os.path.join(BASE_DIR, 'my_project')
-templates_new_path = os.path.join(my_project_path, 'templates')
 for root, dirs, files in os.walk(my_project_path):
-    for i in range(len(dirs)):
-        if dirs[i] == 'templates':
-            shutil.copytree(os.path.join(root, dirs[i]), os.path.join(BASE_DIR, 'templates'), dirs_exist_ok=True)
+    for dir in dirs:
+        if dir == 'templates':
+            shutil.copytree(os.path.join(root, dir), os.path.join(BASE_DIR, 'templates'), dirs_exist_ok=True)
