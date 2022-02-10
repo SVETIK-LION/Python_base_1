0fb2fb62f839f1cac87dc9dc4fdfe0a1653364d6
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-11 00:37:56 +0500

    task_7_4.py is working

--- Klepikova_Svetlana_dz_7/task_7_4.py
+++ Klepikova_Svetlana_dz_7/task_7_4.py
@@ -1,6 +1,6 @@
 import os.path
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-folder = os.path.join(BASE_DIR, 'my_project')
+
+
 border_1 = 10
 border_2 = 100
 border_3 = 1000
@@ -9,16 +9,18 @@ count_1 = 0
 count_2 = 0
 count_3 = 0
 count_4 = 0
-for item in os.listdir(folder):
-    file_size = os.stat(os.path.join(folder, item)).st_size
-    if file_size <= border_1:
-        count_1 += 1
-    elif file_size <= border_2:
-        count_2 += 1
-    elif file_size <= border_3:
-        count_3 += 1
-    elif file_size <= border_4:
-        count_4 += 1
+for root, dirs, files in os.walk('my_project'):
+    if files:
+        for file in files:
+            file_size = os.stat(os.path.join(root, file)).st_size
+            if file_size <= border_1:
+                count_1 += 1
+            elif border_1 < file_size <= border_2:
+                count_2 += 1
+            elif border_2 < file_size <= border_3:
+                count_3 += 1
+            elif border_3 < file_size <= border_4:
+                count_4 += 1
 dict_size = {border_1: count_1,
              border_2: count_2,
              border_3: count_3,
