1ada397bc537f71ff559cb7eec9520caae22b65f
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-17 02:19:20 +0500

    change task_9_1.py add task_9_4.py

--- Klepikova_Svetlana_dz_9/task_9_1.py
+++ Klepikova_Svetlana_dz_9/task_9_1.py
@@ -7,21 +7,21 @@ class TrafficLight:
 
     def running(self, timing = 4):
         if timing == 4:
-            __color = 'red'
-            print(f'{__color} {timing} сек')
+            TrafficLight.__color = 'red'
+            print(f'{TrafficLight.__color} {timing} сек')
             time.sleep(4)
             timing -= 2
 
         if timing == 2:
-            __color = 'yellow'
-            print(f'{__color} {timing} сек')
+            TrafficLight.__color = 'yellow'
+            print(f'{TrafficLight.__color} {timing} сек')
             time.sleep(2)
             timing += 1
 
 
         if timing == 3:
-            __color = 'green'
-            print(f'{__color} {timing} сек')
+            TrafficLight.__color = 'green'
+            print(f'{TrafficLight.__color} {timing} сек')
             time.sleep(3)
             timing -= 1
 
