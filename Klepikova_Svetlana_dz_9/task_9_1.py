5a0fac21f71a2c434ff3ea450acd85f36062d598
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-17 03:21:08 +0500

    change task_9_1.py

--- Klepikova_Svetlana_dz_9/task_9_1.py
+++ Klepikova_Svetlana_dz_9/task_9_1.py
@@ -9,9 +9,8 @@ class TrafficLight:
         colors = {4: 'red', 2: 'yellow', 3: 'green'}
         for key, val in colors.items():
             TrafficLight.__color = val
-            for sec in range(0, key + 1):
-                sys.stdout.write('\r')
-                sys.stdout.write(f'{TrafficLight.__color} {sec} сек')
+            for sec in range(1, key + 1):
+                sys.stdout.write(f'\r{TrafficLight.__color} {sec} сек')
                 time.sleep(1)
             print()
 
