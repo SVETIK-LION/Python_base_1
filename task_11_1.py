49d3176ce807d828cd3e0cc6122c0d07de8a7018
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-02-28 10:27:36 +0500

    task_11_1.py is working

--- task_11_1.py
+++ task_11_1.py
@@ -1,33 +1,36 @@
 class Date:
     def __init__(self, date: str):
         self.date = date
-        self.type_int = type_int
-        self.valid_date = valid_date
 
 
-        @classmethod
-        def type_int(cls, date):
-            cls.date = date
-            day, month, year = int(date.split('-'))
-            print(f'Мы разделили дату на числа: {day}, {month}, {year}')
+    @classmethod
+    def type_int(cls, date):
+        cls.date = date
+        day, month, year = date.split('-')
+        print(f'Мы разделили дату на числа: {day}, {month}, {year}')
+        s = f'{day} {month} {year}'
+        return s
 
 
-        @staticmethod
-        def valid_date(day: int, month: int, year: int):
-            if day < 1 or day > 31:
-                raise ValueError(f'Неверное значение: {day}')
-            elif month < 1 or month > 12:
-                raise ValueError(f'Неверное значение: {month}')
-            elif month == 2 and day > 28:
-                raise ValueError(f'Неверное значение: {day}')
-            elif len(year) < 4 or len(year) > 4:
-                raise ValueError(f'Неверное значение: {year}')
-            else:
-                print('Все хорошо, дата правильная :)')
+    @staticmethod
+    def valid_date(s):
+        day, month, year = s.split(' ')
+        day, month = int(day), int(month)
+        if day < 1 or day > 31:
+            raise ValueError(f'Неверное значение: {day}')
+        elif month < 1 or month > 12:
+            raise ValueError(f'Неверное значение: {month}')
+        elif month == 2 and day > 28:
+            raise ValueError(f'Неверное значение: {day}')
+        elif len(year) < 4 or len(year) > 4:
+            raise ValueError(f'Неверное значение: {year}')
+        else:
+            print('Все хорошо, дата правильная :)')
 
 
-#????????????????????????
 if __name__ == '__main__':
-    d = Date('22-12-2022')
-    print(Date.type_int)
-    print(Date.valid_date)
+    try:
+        d = Date.type_int('28-02-2022')
+        Date.valid_date(d)
+    except ValueError as err:
+        print(err)
